1a4519ec25e34302dcebb2d93f215237a8ed9d81
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-02-06 19:02:28 -0300

    atualização estrutura banco de dados

--- create-database.sql
+++ create-database.sql
@@ -1,14 +1,35 @@
--- drop schema curso cascade;
+DROP TABLE IF EXISTS "curso"."transacoes";
+DROP TABLE IF EXISTS "curso"."contas";
+DROP TABLE IF EXISTS "curso"."pessoas";
+DROP SCHEMA IF EXISTS "curso";
 
-create schema curso;
+CREATE SCHEMA "curso";
 
-create table curso.contas (
-  conta_id uuid primary key,
-  nome_completo text not null,
-  usuario text not null,
-  senha text not null,
-  agencia text not null,
-  numero_conta text not null,
-  tipo_conta char not null,
-  saldo double precision not null
+CREATE TABLE "curso"."pessoas" (
+  "pessoa_id" uuid PRIMARY KEY,
+  "nome_completo" text NOT NULL,
+  "cpf" text NOT NULL,
+  "usuario" text NOT NULL,
+  "senha" text NOT NULL
 );
+
+CREATE TABLE "curso"."contas" (
+  "conta_id" uuid PRIMARY KEY,
+  "pessoa_id" uuid NOT NULL,
+  "agencia" text NOT NULL,
+  "numero_conta" text NOT NULL,
+  "tipo_conta" char NOT NULL,
+  "saldo" double precision NOT NULL
+);
+
+CREATE TABLE "curso"."transacoes" (
+  "transacao_id" uuid PRIMARY KEY,
+  "conta_id" uuid NOT NULL,
+  "tipo" char NOT NULL,
+  "valor" double precision NOT NULL,
+  "data_hora_transacao" timestamp
+);
+
+ALTER TABLE "curso"."contas" ADD FOREIGN KEY ("pessoa_id") REFERENCES "curso"."pessoas" ("pessoa_id");
+
+ALTER TABLE "curso"."transacoes" ADD FOREIGN KEY ("conta_id") REFERENCES "curso"."contas" ("conta_id");
