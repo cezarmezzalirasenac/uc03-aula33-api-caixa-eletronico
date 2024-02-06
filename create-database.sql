d70ba8401cd3a640db1414f6e07b7c812187b1b6
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-02-06 19:14:38 -0300

    adicionado privilegio

--- create-database.sql
+++ create-database.sql
@@ -1,19 +1,20 @@
-DROP TABLE IF EXISTS "curso"."transacoes";
-DROP TABLE IF EXISTS "curso"."contas";
-DROP TABLE IF EXISTS "curso"."pessoas";
-DROP SCHEMA IF EXISTS "curso";
+DROP TABLE IF EXISTS "banco"."transacoes";
+DROP TABLE IF EXISTS "banco"."contas";
+DROP TABLE IF EXISTS "banco"."pessoas";
+DROP SCHEMA IF EXISTS "banco";
 
-CREATE SCHEMA "curso";
+CREATE SCHEMA "banco";
 
-CREATE TABLE "curso"."pessoas" (
+CREATE TABLE "banco"."pessoas" (
   "pessoa_id" uuid PRIMARY KEY,
   "nome_completo" text NOT NULL,
   "cpf" text NOT NULL,
   "usuario" text NOT NULL,
-  "senha" text NOT NULL
+  "senha" text NOT NULL,
+  "privilegio" text NOT NULL DEFAULT 'comum'
 );
 
-CREATE TABLE "curso"."contas" (
+CREATE TABLE "banco"."contas" (
   "conta_id" uuid PRIMARY KEY,
   "pessoa_id" uuid NOT NULL,
   "agencia" text NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE "curso"."contas" (
   "saldo" double precision NOT NULL
 );
 
-CREATE TABLE "curso"."transacoes" (
+CREATE TABLE "banco"."transacoes" (
   "transacao_id" uuid PRIMARY KEY,
   "conta_id" uuid NOT NULL,
   "tipo" char NOT NULL,
@@ -30,6 +31,6 @@ CREATE TABLE "curso"."transacoes" (
   "data_hora_transacao" timestamp
 );
 
-ALTER TABLE "curso"."contas" ADD FOREIGN KEY ("pessoa_id") REFERENCES "curso"."pessoas" ("pessoa_id");
+ALTER TABLE "banco"."contas" ADD FOREIGN KEY ("pessoa_id") REFERENCES "banco"."pessoas" ("pessoa_id");
 
-ALTER TABLE "curso"."transacoes" ADD FOREIGN KEY ("conta_id") REFERENCES "curso"."contas" ("conta_id");
+ALTER TABLE "banco"."transacoes" ADD FOREIGN KEY ("conta_id") REFERENCES "banco"."contas" ("conta_id");
